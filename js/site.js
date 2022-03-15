464b4296a2fc36abed730c34c6a2ff37e80c9cb8
thomaspereira94 <thomaspereira1994@outlook.com>
2022-03-15 13:51:10 -0300

    done

--- js/site.js
+++ js/site.js
@@ -1,4 +1,24 @@
-//call hello world
-function HelloWorld(){
-    alert("hello world");
+//Controller Function
+function controllerFunction() {
+    //getUserInput();
+    let userInput = document.getElementById("userInput").value;
+
+    let reversedString = reverseString(userInput);
+
+    displayResult(reversedString);
+}
+
+//Function to reverse string
+function reverseString(userInput) {
+    let revString = [];
+
+    for (let index = userInput.length - 1; index >= 0; index--) {
+        revString += userInput[index];
+    }
+    return revString;
+}
+
+//Function to display result
+function displayResult(reversedString) {
+    document.getElementById("resultDisplayArea").value = reversedString;
 }
